66d025cb3ff36f80314806c8de7af423a8a60227
Renu P Peter <20105518@mail.wit.ie>
2024-08-14 19:03:59 +0100

    Issues based on Feature branch

---
+++ target/classes/vue/views/image-overview.vue
@@ -0,0 +1,33 @@
+<template id="image-overview">
+  <app-layout>
+    <div class="card bg-light mb-3">
+      <div class="card-header">
+        <div class="row">
+          <div class="col-6">
+            Images
+          </div>
+          <div class="col" align="right">
+            <button rel="tooltip" title="Add"
+                    class="btn btn-info btn-simple btn-link"
+                    @click="hideForm = !hideForm">
+              <i class="fa fa-plus" aria-hidden="true"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="card-body" :class="{ 'd-none': hideForm}">
+        <form id="addImage">
+
+        </form>
+      </div>
+    </div>
+  </app-layout>
+</template>
+
+<script>
+
+app.component("image-overview", {
+  template: "#image-overview",
+});
+
+</script>
